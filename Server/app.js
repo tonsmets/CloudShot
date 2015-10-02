bd4bb2162c5d8a1e4264db7adee9cc1c5292fd39
Ton Smets <info@tonsmets.nl>
2015-10-02 16:55:48 +0200

    Added security fixes and made the screenshots GET route cleaner

--- Server/app.js
+++ Server/app.js
@@ -7,6 +7,8 @@ var fs = require('fs');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 var uuid = require('node-uuid');
+var readChunk = require('read-chunk');
+var imageType = require('image-type');
 
 var config = require('./config'); // Load the config
 var auth = require('./auth'); // Load some authentication middleware
@@ -22,14 +24,104 @@ app.use(bodyParser.json());
 app.use(morgan('short'));
 
 fs.open(path.join(__dirname, "uploads/"),'r',function(err,fd){
-    if (err && err.code=='ENOENT') { 
-	    fs.mkdirSync(path.join(__dirname, "uploads/"));
-	    console.log("Uploads folder created");
+	if (err && err.code=='ENOENT') { 
+		fs.mkdirSync(path.join(__dirname, "uploads/"));
+		console.log("Uploads folder created");
 	}
-    else {
-    	console.log("Uploads folder exists");
-    }
+	else {
+		console.log("Uploads folder exists");
+	}
 });
+
+// API ROUTES //
+var apiRoutes = express.Router(); 
+
+apiRoutes.use('/screenshots', auth.checkToken);
+apiRoutes.use('/upload', auth.checkToken);
+
+apiRoutes.post('/auth', function(req, res) {
+	if(req.body.userid == config.userKey) {
+		var token = jwt.sign({ userid: config.userKey }, config.serverSecret, {
+			expiresInMinutes: 720 // 12 hours till expiration
+		});
+
+		res.json({
+			success: true,
+			message: 'Generating token succeeded',
+			token: token
+		});
+	}
+});
+
+apiRoutes.get('/screenshots', function(res, res) {
+	screenshots.find({}, function (err, doc) {
+		if(err) {
+			console.log("[/upload] - ERROR: " + err);
+		}
+		else {
+			var tmpArr = [];
+			for(var i = 0; i < doc.length; i++) {
+				var tmpObj = {};
+				tmpObj.uniqueId = doc[i]._id;
+				tmpObj.uploadDate = doc[i].uploadDate;
+				tmpObj.url = "/" + doc[i].uid + "/" + doc[i].origFilename;
+				tmpObj.filename = doc[i].origFilename;
+				tmpArr.push(tmpObj);
+			}
+			res.json(tmpArr);
+		}
+	});
+});
+
+apiRoutes.post('/upload', multipartMiddleware, function(req, res) {
+	var buffer = readChunk.sync(req.files.screenFile.path, 0, 12);
+	var type = imageType(buffer);
+
+	if(type && (type.ext == 'jpg' || type.ext == 'png') && (type.mime == 'image/png' || type.mime == 'image/jpg')) {
+		var uniqueId = uuid.v4();
+		var newFileName = uniqueId + "-" + req.files.screenFile.originalFilename;
+
+		var currentPath = req.files.screenFile.path;
+		var newPath = path.join(__dirname, "uploads/") + newFileName;
+
+		var is = fs.createReadStream(currentPath);
+		var os = fs.createWriteStream(newPath);
+
+		is.pipe(os);
+		is.on('end',function() {
+			// Delete the temp file
+			fs.unlinkSync(currentPath);
+			var newScreenshot = {
+				origFilename: req.files.screenFile.originalFilename,
+				filename: newFileName,
+				uid: uniqueId,
+				uploadDate: new Date()
+			}
+
+			screenshots.insert(newScreenshot, function(err, doc) {
+				if(err) {
+					res.json({ success: false, message: 'Failed to store screenshot. Database error'});
+				}
+				else {
+					res.json({ success: true, message: 'Uploaded file', url: "/" + uniqueId + "/" + req.files.screenFile.originalFilename});
+				}
+			});
+		});
+
+		is.on('error', function(err) {
+			console.log("Error in file I/O: " + err);
+			res.json({ success: false, message: 'Failed to store screenshot. File I/O error'});
+		});
+	}
+	else {
+		res.json({ success: false, message: 'Unsupported file format'});
+	}
+
+	
+});
+
+// Prefix all API routes with /api
+app.use('/api', apiRoutes);
 
 app.get('/', function(req, res) {
 	res.send("Hello!");
@@ -52,77 +144,6 @@ app.get('/:uuid/:filename', function (req, res) {
 		}
 	});
 });
-
-// API ROUTES //
-var apiRoutes = express.Router(); 
-
-apiRoutes.use('/screenshots', auth.checkToken);
-apiRoutes.use('/upload', auth.checkToken);
-
-apiRoutes.post('/auth', function(req, res) {
-	if(req.body.userid == config.userKey) {
-		var token = jwt.sign({ userid: config.userKey }, config.serverSecret, {
-			expiresInMinutes: 720 // 12 hours till expiration
-        });
-
-        res.json({
-			success: true,
-			message: 'Generating token succeeded',
-			token: token
-        });
-	}
-});
-
-apiRoutes.get('/screenshots', function(res, res) {
-	screenshots.find({}, function (err, doc) {
-		if(err) {
-			console.log("[/upload] - ERROR: " + err);
-		}
-		else {
-			res.json(doc);
-		}
-	});
-});
-
-apiRoutes.post('/upload', multipartMiddleware, function(req, res) {
-	var uniqueId = uuid.v4();
-	var newFileName = uniqueId + "-" + req.files.screenFile.originalFilename;
-
-	var currentPath = req.files.screenFile.path;
-	var newPath = path.join(__dirname, "uploads/") + newFileName;
-
-	var is = fs.createReadStream(currentPath);
-	var os = fs.createWriteStream(newPath);
-
-	is.pipe(os);
-	is.on('end',function() {
-		// Delete the temp file
-	    fs.unlinkSync(currentPath);
-	    var newScreenshot = {
-			origFilename: req.files.screenFile.originalFilename,
-			filename: newFileName,
-			uid: uniqueId,
-			uploadDate: new Date()
-		}
-
-		screenshots.insert(newScreenshot, function(err, doc) {
-			if(err) {
-				res.json({ success: false, message: 'Failed to store screenshot. Database error'});
-			}
-			else {
-				res.json({ success: true, message: 'Uploaded file', url: "/" + uniqueId + "/" + req.files.screenFile.originalFilename});
-			}
-		});
-	});
-
-	is.on('error', function(err) {
-		console.log("Error in file I/O: " + err);
-		res.json({ success: false, message: 'Failed to store screenshot. File I/O error'});
-	});
-});
-
-// Prefix all API routes with /api
-app.use('/api', apiRoutes);
 
 app.listen(port);
 console.log("Server is running on port " + port);
